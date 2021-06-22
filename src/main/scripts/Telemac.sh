7176ef2f023615cd26da95f6f0d9a2d85bad17f1
Yann Richet <richetyann@gmail.com>
2021-06-22 14:17:13 +0200

    Update Telemac.sh

--- src/main/scripts/Telemac.sh
+++ src/main/scripts/Telemac.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ 1 -le `echo \`pwd -P\` | grep --color='auto' -P -n "[\x80-\xFF]" | wc -l` ]; then
+  echo "Telemac will not support non ISO char in path. Exiting."; 
+  exit 1
+fi
+
 . $HOME/opt/telemac-mascaret/v7p3r1/telemac.profile
 
 telemac2d.py -c ubugfmpich2 $* &
