47b99d505ed5e7bb481e7b94659245c382e50104
yannrichet <richetyann@gmail.com>
2021-09-13 09:40:52 +0200

    this will prevent . */telemac.profile to corrupt $* for telemac2d.py

--- src/main/scripts/Telemac.sh
+++ src/main/scripts/Telemac.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CAS=$1
+
 if [ 1 -le `echo \`pwd -P\` | grep --color='auto' -P -n "[\x80-\xFF]" | wc -l` ]; then
   echo "Telemac will not support non ISO char in path. Exiting."; 
   exit 1
@@ -7,7 +9,7 @@ fi
 
 . $HOME/opt/telemac-mascaret/v7p3r1/telemac.profile
 
-telemac2d.py -c ubugfmpich2 $* &
+telemac2d.py -c ubugfmpich2 $CAS &
 PID=$!
 echo $PID >> PID
 wait $PID
