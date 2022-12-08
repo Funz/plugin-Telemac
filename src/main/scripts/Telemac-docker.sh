dcb054ff53ed77dd14a029249c254c27783fa81a
Yann Richet <yann.richet@laposte.net>
2022-12-08 17:33:23 +0100

    use irsn docker

--- src/main/scripts/Telemac-docker.sh
+++ src/main/scripts/Telemac-docker.sh
@@ -9,7 +9,7 @@ fi
 
 NCPU=`grep ^cpu\\\\scores /proc/cpuinfo | uniq |  awk '{print $4}'`
 
-docker run `echo $PWD`:/tmp nicogodet/telemac:v8p2r1 bash -c "source ../setenv.sh && cd /tmp && telemac2d.py --ncsize=$NCPU $CAS" &
+docker run `echo $PWD`:/workdir irsn/telemac-mascaret:latest telemac2d.py --ncsize=$NCPU $CAS &
 
 PID=$!
 echo $PID >> PID
--- src/main/scripts/Telemac-singularity.sh
+++ src/main/scripts/Telemac-singularity.sh
@@ -10,10 +10,10 @@ fi
 NCPU=`grep ^cpu\\\\scores /proc/cpuinfo | uniq |  awk '{print $4}'`
 
 ## singularity run using docker image:
-singularity run -B `echo $PWD`:/tmp docker://nicogodet/telemac:v8p2r1 bash -c "source ../setenv.sh && cd /tmp && telemac2d.py --ncsize=$NCPU $CAS" &
-## singularity run using singularity image (faster within SLURM): 1st, move docker image to singularity with `singularity build --sandbox telemac-v8p2r1 docker://nicogodet/telemac:v8p2r1`
+singularity run -B `echo $PWD`:/workdir -H /workdir docker://irsn/telemac-mascaret:latest telemac2d.py --ncsize=$NCPU $CAS &
+## singularity run using singularity image (faster within SLURM): 1st, move docker image to singularity with `singularity build --sandbox telemac-mascaret docker://irsn/telemac-mascaret:latest`
 #module load singularity
-#singularity run -B `echo $PWD`:/tmp telemac-v8p2r1 bash -c "source ../setenv.sh && cd /tmp && telemac2d.py --ncsize=$NCPU $CAS" &
+#singularity run -B `echo $PWD`:/workdir -H /workdir telemac-mascaret telemac2d.py --ncsize=$NCPU $CAS &
 
 PID=$!
 echo $PID >> PID
