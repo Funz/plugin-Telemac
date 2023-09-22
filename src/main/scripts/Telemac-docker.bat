bde06ec135dc75c34c5996eb17d94a46ba1b8fc5
Yann Richet <richetyann@gmail.com>
2023-09-22 11:25:16 +0200

    Update Telemac-docker.bat

--- src/main/scripts/Telemac-docker.bat
+++ src/main/scripts/Telemac-docker.bat
@@ -4,7 +4,11 @@ CAS=%1%
 
 NCPU=%NUMBER_OF_PROCESSORS%
 
-rem assume proxy is ok (export https_proxy="http://login:passwd@proxy-ip:port"), or docker pull irsn/telemac-mascaret was already done.
+rem assume:
+rem  1. WSL2 is installed on your Windows: "wsl.exe --install", then reboot, then "wsl.exe --set-defaut-version 2"
+rem  2. Debian distro is installed in WSL2: "wsl.exe --install -d Debian"
+rem  3. if behind proxy, within Debian: "export https_proxy="http://login:passwd@proxy-ip:port", and "docker pull irsn/telemac-mascaret" to pre-install Telemac.
+rem Then, following run should work:
 wsl -u root -e bash -c "docker run -v `echo $PWD`:/workdir irsn/telemac-mascaret:latest telemac2d.py --ncsize=%NCPU% %CAS%"
 
 for /F "TOKENS=1,2,*" %%a in ('tasklist /FI "IMAGENAME eq wsl.exe"') do set PID_WSL=%%b
